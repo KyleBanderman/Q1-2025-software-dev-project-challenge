7d562b5e8e053bf613dc38a36289c941cab91f73
Kyle Banderman <kyle.banderman4675@gmail.com>
2025-01-16 15:20:11 -0600

    Added initial framework for project. Frontend will be React with Next.js, and backend will be Java with Spring Boot

---
+++ backend/Q1-Software/src/main/java/org/launchcode/Q1_Software/Q1SoftwareApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Q1_Software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Q1SoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Q1SoftwareApplication.class, args);
+	}
+
+}
---
+++ backend/Q1-Software/src/test/java/org/launchcode/Q1_Software/Q1SoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Q1_Software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Q1SoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
